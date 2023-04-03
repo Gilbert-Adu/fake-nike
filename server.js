195d9e8f6a7bc6beb60ddb7b89d6e125fa07fbe3
Gilbert Adu <gea33@cornell.edu>
2023-04-02 21:58:05 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,4 +4,7 @@ const path = require('path');
 app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
 const port = process.env.PORT || 3000;
+app.get("/", (req, res) => {
+  res.sendFile("./src/App.vue");
+});
 app.listen(port);
